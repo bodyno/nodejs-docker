7a4de200bd75de1cd2198a2d28b1c33084e013ce
bodyno <411298027@qq.com>
2017-09-26 14:59:40 +0800

    add docker-compose.yml

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const os = require("os");
+const express = require('express')
+const os = require('os')
+const Redis = require('ioredis')
+
+const redis = new Redis({
+  host: 'redis'
+})
 
 // Constants
-const PORT = 8080;
+const PORT = 8080
 
 // App
-const app = express();
+const app = express()
 app.get('/', function (req, res) {
-  res.send('Hello world ' + os.hostname());
-});
+  const counter = redis.incr('counter')
+  console.log(counter)
+  res.send(`Hello world ${os.hostname()}  redis: ${counter}`)
+})
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+app.listen(PORT)
+console.log('Running on http://localhost:' + PORT)
