b8a34c25fd92986079aeea232bbe6990c2a5ded8
bodyno <411298027@qq.com>
2017-09-27 11:06:21 +0800

    add mysql mq

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const os = require('os')
 const Redis = require('ioredis')
+const mysql = require('mysql')
+const amqp = require('amqplib')
+
+amqp.connect('amqp://mq').then((conn) => {
+  console.log('mq connected')
+}).catch(err => {
+  console.log('mq failed')
+  console.log(err)
+})
 
 const redis = new Redis({
   host: 'redis'
+})
+
+const connection = mysql.createConnection('mysql://root:xuzeyu123@mysql/nobody_db')
+
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+    return
+  }
+
+  console.log('connected as id ' + connection.threadId)
 })
 
 // Constants
