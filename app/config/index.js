f3964b7927b23d16ec523c3b70d75871a0d683e2
bodyno <411298027@qq.com>
2017-09-28 10:47:19 +0800

    optimize

--- app/config/index.js
+++ app/config/index.js
@@ -1,10 +1,19 @@
-let config = {
-  mq: 'amqp://root:xuzeyu123@localhost',
-  mysql: 'mysql://root:xuzeyu123@localhost/nobody_db',
-  redis: 'redis://localhost:6379',
-}
-if (process.env.NODE_ENV == 'dev') {
+let config = {}
 
+if (process.env.NODE_ENV == 'dev') {
+  config = {
+    mq: 'amqp://root:xuzeyu123@localhost',
+    mysql: 'mysql://root:xuzeyu123@localhost/nobody_db',
+    redis: 'redis://localhost:6379',
+  }
+}
+
+if (process.env.NODE_ENV == 'prod') {
+  config = {
+    mq: 'amqp://root:xuzeyu123@mq',
+    mysql: 'mysql://root:xuzeyu123@mysql/nobody_db',
+    redis: 'redis://redis',
+  }
 }
 
 module.exports = config
--- app/server.js
+++ app/server.js
@@ -5,23 +5,30 @@ const mysql = require('mysql')
 const amqp = require('amqplib')
 const config = require('./config')
 
-amqp.connect(config.mq).then((conn) => {
-  console.log('mq connected')
-}).catch(err => {
-  console.log('mq failed')
-})
-
 const redis = new Redis(config.redis)
 
 const connection = mysql.createConnection(config.mysql)
+function connectMysql () {
+  connection.connect((err) => {
+    if (err) {
+      console.error('mysql failed')
+      setTimeout(connectMysql, 1000)
+      return
+    }
+    console.log('mysql connected')
+  })
+}
+connectMysql()
 
-connection.connect((err) => {
-  if (err) {
-    console.error('error connecting: ' + err.stack)
-    return
-  }
-  console.log('connected as id ' + connection.threadId)
-})
+function connectMq () {
+  amqp.connect(config.mq).then(conn => {
+    console.log('mq connected')
+  }).catch(err => {
+    console.error('mq failed')
+    setTimeout(connectMq, 1000)
+  })
+}
+connectMq()
 
 // Constants
 const PORT = 8080
